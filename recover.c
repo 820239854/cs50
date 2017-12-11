2a06a84ec632b44f8599cdea297c18e188c51ce4
shiyanpin <820239854@qq.com>
2017-12-11 21:35:40 +0800

    have found the header

--- recover.c
+++ recover.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<stdlib.h>
+
 
 int main(int argc, char* argv[])
 {
@@ -19,60 +19,44 @@ int main(int argc, char* argv[])
     FILE *output = fopen("test.jpg", "w");
     if(output==NULL)
     {
-        fclose(file);
         fprintf(stderr, "Counld'd write\n.");
         return 2;
     }
 
-    // printf("%lu", sizeof(char));
-    int change = 0;
+    // printf("%lu", sizeof(0xff));
+
+    int to_seek=0;
+    // while(to_seek != EOF)
+    // printf("%lu\n", sizeof(0xff));
+    // printf("%lu\n", sizeof(0xf));
+
+    // for(int j=0; j<3000; j++)
+    int num = 0;
     while(!feof(file))
-    // for(int k=0; k<200; k++)
     {
-        char* content = (char*)malloc(512);
-
-        fread(content, 512, 1 ,file);
-
-        if( (int)content[0] == 0xff)
+        fread(&to_seek,1,1,file);
+        if(to_seek == 0xff)
         {
-            if( (int)content[1] == 0xd8)
-            {
-                if( (int)content[2] == 0xff)
+            printf("%x\n", to_seek);
+            fread(&to_seek,1,1,file);
+                if(to_seek == 0xd8)
                 {
-                    if( (int)content[3] >= 0xe0 && (int)content[3]<= 0xef)
-                    {
-                        // for(int wa=0; wa<4; wa++)
-                        // {
-                        //     fwrite( content+wa ,sizeof(char) ,1,output);
-                        // }
-                        for(int j=0; j<512; j++)
+                    printf("%x\n", to_seek);
+                    fread(&to_seek,1,1,file);
+                        if( to_seek == 0xff)
                         {
-                            for(int num=0; num<16; num++)
+                            printf("%x\n", to_seek);
+                            fread(&to_seek,1,1,file);
+                            if(to_seek >= 0xe0 &&  to_seek<= 0xef)
                             {
-                                printf("%x", content[j]);
+                               printf("%x\n", to_seek);
+                               printf("%d", num);
+                               break;
                             }
-                            printf("\n");
                         }
-                        if(change == 0)
-                            change = 1;
-                        else
-                            break;
-                    }
                 }
-            }
         }
-        if(change == 1)
-            fwrite(content, 512, 1 , output);
-        // for(int j=0; j<512; j++)
-        // {
-        //     for(int num=0; num<16; num++)
-        //     {
-        //         printf("%x", content[j]);
-        //     }
-        //     printf("\n");
-        // }
-        // free(content);
-        // printf("------------------------ %d ------------\n", k);
+        num++;
     }
     fclose(file);
     fclose(output);
